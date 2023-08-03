6573896421801520ee593e07e4bae736a6a6eed4
Kevin Lee <kevin.code@kevinlee.io>
2023-08-03 22:10:52 +1000

    Add more 'use enum' example

--- src/custom_types/enums/use_enums.rs
+++ src/custom_types/enums/use_enums.rs
@@ -4,6 +4,16 @@ enum Status {
   Inactive,
 }
 
+enum Week {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
 pub fn use_enums() -> () {
   use Status::{Active, Inactive};
 
@@ -12,6 +22,18 @@ pub fn use_enums() -> () {
 
   check_status(active);
   check_status(inactive);
+
+  say_week_day(Week::Monday);
+  // say_week_day(Monday); // compile-time error: not found in this scope
+  {
+    use Week::*;
+    say_week_day(Tuesday);
+    say_week_day(Wednesday);
+    say_week_day(Thursday);
+    say_week_day(Friday);
+    say_week_day(Saturday);
+    say_week_day(Sunday);
+  }
 }
 
 fn check_status(status: Status) -> () {
@@ -20,4 +42,19 @@ fn check_status(status: Status) -> () {
     Active => println!("Status: {:?}", Active),
     Inactive => println!("Status: {:?}", Inactive),
   }
+}
+
+fn say_week_day(week: Week) -> () {
+  use Week::*;
+
+  let day = match week {
+    Monday => "Monday",
+    Tuesday => "Tuesday",
+    Wednesday => "Wednesday",
+    Thursday => "Thursday",
+    Friday => "Friday",
+    Saturday => "Saturday",
+    Sunday => "Sunday",
+  };
+  println!("Today is {day}.")
 }
