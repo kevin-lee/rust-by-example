6c1089f73979444c8ca3c47a588a9bfc256954f6
Kevin Lee <kevin.code@kevinlee.io>
2023-08-04 22:50:31 +1000

    Add comment to ScalaLikeStringOps::strip_margin

--- src/string_ops.rs
+++ src/string_ops.rs
@@ -1,4 +1,21 @@
 pub trait ScalaLikeStringOps {
+  /// Returns a string slice with leading whitespace and | removed
+  /// It removes the leading whitespace chars then remove the subsequent '|' if found.
+  /// # Examples
+  /// ```
+  /// let s = format!("a=123
+  ///                 |b=456
+  ///                 |c=789
+  ///                 |");
+  /// s.strip_margin();
+  /// ```
+  /// returns
+  /// ```
+  /// "a=123
+  /// b=456
+  /// c=789
+  /// "
+  /// ```
   fn strip_margin(&self) -> String;
 }
 
