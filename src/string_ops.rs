db14f4408ac03aafbb7cd7ec8166f5951a73477e
Kevin Lee <kevin.code@kevinlee.io>
2023-08-06 18:36:32 +1000

    Update: string_ops.rs - Add tests

--- src/string_ops.rs
+++ src/string_ops.rs
@@ -65,3 +65,59 @@ impl<A: AsRef<str>> ScalaLikeStringOps for A {
       .join("\n")
   }
 }
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+
+  #[test]
+  fn strip_margin_for_string() {
+    let s = "a=123
+               |b=456
+               |c=789
+               |";
+    let expected = "a=123\nb=456\nc=789\n";
+
+    let actual = s.strip_margin();
+    assert_eq!(actual, expected)
+  }
+
+  #[test]
+  fn strip_margin_for_string_with_intentional_indent() {
+    let s = "a=123
+               |  b=456
+               |    c=789
+               |";
+    let expected = "a=123\n  b=456\n    c=789\n";
+
+    let actual = s.strip_margin();
+    assert_eq!(actual, expected)
+  }
+
+  #[test]
+  fn strip_margin_for_str() {
+    let s = "a=123
+               |b=456
+               |c=789
+               |";
+    let expected = "a=123\nb=456\nc=789\n";
+
+    let actual = s.strip_margin();
+    assert_eq!(actual, expected)
+  }
+
+  #[test]
+  fn strip_margin_for_str_with_intentional_indent() {
+    let s = "a=123
+               |  b=456
+               |    c=789
+               |";
+    let expected = "a=123\n  b=456\n    c=789\n";
+
+    let actual = s.strip_margin();
+    assert_eq!(actual, expected)
+  }
+
+}
