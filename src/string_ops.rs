f797166c3bff78a8346e025f9e2d1795268df97f
Kevin Lee <kevin.code@kevinlee.io>
2023-08-07 20:26:17 +1000

    Update: string_ops.rs - Fix unit test

--- src/string_ops.rs
+++ src/string_ops.rs
@@ -74,10 +74,10 @@ mod tests {
 
   #[test]
   fn strip_margin_for_string() {
-    let s = "a=123
-               |b=456
-               |c=789
-               |";
+    let s: String = format!("a=123
+                            |b=456
+                            |c=789
+                            |");
     let expected = "a=123\nb=456\nc=789\n";
 
     let actual = s.strip_margin();
@@ -86,10 +86,10 @@ mod tests {
 
   #[test]
   fn strip_margin_for_string_with_intentional_indent() {
-    let s = "a=123
-               |  b=456
-               |    c=789
-               |";
+    let s: String = format!("a=123
+                            |  b=456
+                            |    c=789
+                            |");
     let expected = "a=123\n  b=456\n    c=789\n";
 
     let actual = s.strip_margin();
